b8095992759a35392b026b798f8d81ca3ebc26e4
aatik05 <rah22182@spengergasse.at>
2022-11-06 22:35:49 +0100

    Grundsachen
    06.11.2022

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,7 +1,12 @@
-function register() {
+function register() 
+{
     window.location.assign("kontoErstellen.html")
-  window.location.href = "kontoErstellen.html";
+    window.location.href = "kontoErstellen.html";
 }
-function login() {
-    window.open("main.html");
+
+function login() 
+{
+    window.location.assign("index.html")
+    window.location.href = "index.html";
+    window.open("../html/index.html");
 }
